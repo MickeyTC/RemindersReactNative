d6f9dfcb11187d9d3ebdeb5a2c40f657bbdf3829
Teerapat Chintanakit <teerapat.c@scale360solutions.com>
2018-08-08 11:00:56 +0700

    Add AddButton to NavBar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react'
-import { Platform, StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import TodoList from './components/TodoList'
-import AddButton from './components/AddButton'
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-          <TodoList />
-          <AddButton />
+        <TodoList />
       </View>
     )
   }
--- src/Router.js
+++ src/Router.js
@@ -1,18 +1,64 @@
-import React from 'react'
-import { Scene, Router } from 'react-native-router-flux'
+import React, { Component } from 'react'
+import { Scene, Router, Actions } from 'react-native-router-flux'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import shortid from 'shortid'
 import App from './App'
 import TodoDetail from './components/TodoDetail'
 
-const RouterComponent = () => {
-  return (
-    <Router>
-      <Scene>
-        <Scene key="app" component={App} title="Reminders" initial />
-        <Scene key="addTodo" title="Add Reminder" component={TodoDetail} />
-        <Scene key="editTodo" title="Edit Reminder" component={TodoDetail} />
-      </Scene>
-    </Router>
-  )
+class RouterComponent extends Component {
+  static propTypes = {
+    addTodo: PropTypes.func,
+  }
+
+  onRight = () => {
+    const todo = {
+      id: shortid.generate(),
+      title: '',
+      description: '',
+      date: '',
+      completed: false,
+    }
+    Actions.addTodo({ todo, onSave: this.props.addTodo })
+  }
+
+  render() {
+    return (
+      <Router>
+        <Scene>
+          <Scene
+            key="app"
+            component={App}
+            title="Reminders"
+            initial
+            rightTitle="Add"
+            onRight={this.onRight}
+          />
+          <Scene
+            key="addTodo"
+            component={TodoDetail}
+            title="Add Reminder"
+            backTitle="Cancel"
+          />
+          <Scene
+            key="editTodo"
+            component={TodoDetail}
+            title="Edit Reminder"
+            backTitle="Cancel"
+          />
+        </Scene>
+      </Router>
+    )
+  }
 }
 
-export default RouterComponent
+const mapDispatchToProps = dispatch => {
+  return {
+    addTodo: todo => dispatch(addTodo(todo)),
+  }
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(RouterComponent)
